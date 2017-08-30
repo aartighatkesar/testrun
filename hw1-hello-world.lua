292f276fce0aadc2f3b726e6c0f5dfd29d988a41
Aarti Ghatkesar <31072745+aartighatkesar@users.noreply.github.com>
2017-08-30 19:34:35 -0400

    Add files via upload

---
+++ hw1-hello-world.lua
@@ -0,0 +1,9 @@
+-------------------------------------------------------------------------------
+--	Filename: helloWorld.lua
+--	Author: Jared Johansen
+-------------------------------------------------------------------------------
+
+print("Hello World!")
+print("I am about to code something awesome!")
+
+
